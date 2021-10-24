f35e7e69c6604de4cfae69426e54b5cbb29b79fd
tombo1818 <tombo1818.dev01@gmail.com>
2021-10-24 10:58:32 +0900

    update & add packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,42 @@
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
+    '@nuxtjs/eslint-config-typescript',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    // 'prettier/vue',
+    'airbnb-base',
+    'prettier',
+    'plugin:import/recommended',
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-  }
+  plugins: ['prettier'],
+  settings: {
+    'import/core-modules': ['vue', 'vuex', 'axios'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      },
+    ],
+    'import/resolver': {
+      'babel-module': {
+        root: '.',
+        alias: {
+          '~': './src',
+          '@': './src',
+          '~~': '.',
+          '@@': '.',
+        },
+      },
+    },
+  },
 }
